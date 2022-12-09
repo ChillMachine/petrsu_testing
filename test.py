93ad1927925e341638194f8a231f241f7643eb55
ChillMachine <67153108+ChillMachine@users.noreply.github.com>
2022-12-09 20:03:30 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ class TestEquation(unittest.TestCase):
     def setUp(self):
         self.Equation = Equation()
 
+    #Все случаи с аргументами != 0
+
     def test_D_biggger_0(self):
         self.assertEqual(self.Equation.get_results_Q_equation(1, 2, -3), [-3.0, 1.0])
     
@@ -17,5 +19,25 @@ class TestEquation(unittest.TestCase):
     def test_D_less_0(self):
         self.assertEqual(self.Equation.get_results_Q_equation(3, 1, 2), 'Нет корней')
 
+    #Аргументы 0
+
+    def test_a0(self):
+        self.assertEqual(self.Equation.get_results_Q_equation(a = 0, b = 1, c = 2), 'a = 0, уравнение не квадратное')
+    
+    def test_b0(self):
+        self.assertEqual(self.Equation.get_results_Q_equation(a = 2, b = 0, c = -2), [-1.0, 1.0])
+
+    def test_b0_c0(self):
+        self.assertEqual(self.Equation.get_results_Q_equation(a = 1, b = 0, c = 0), 0.0)
+
+    def test_c0(self):
+        self.assertEqual(self.Equation.get_results_Q_equation(a = 2, b = 4, c = 0), [-2.0, 0.0])
+
+    def test_c0(self):
+        self.assertEqual(self.Equation.get_results_Q_equation(a = 0, b = 0, c = 0), 'a = 0, уравнение не квадратное')
+
+    #_________________________________________________________________________________
+
+  
 if __name__ == "__main__":
     unittest.main()
