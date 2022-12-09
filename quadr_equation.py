fb1945e52812f76dcdb73f26784df881bb5cf2b0
ChillMachine <67153108+ChillMachine@users.noreply.github.com>
2022-12-09 20:09:24 +0300

    Add files via upload

--- quadr_equation.py
+++ quadr_equation.py
@@ -1,14 +1,13 @@
 class Equation:
 
     def get_results_Q_equation(self, a, b, c):
+        if a == 0: 
+            return 'a = 0, уравнение не квадратное'
         D = b**2 - 4 * a * c
         if D < 0:
             return 'Нет корней'
         elif D == 0:
             return -b / (2 * a)
-
         elif D > 0:
             return sorted([(-b + D**(1/2)) / (2 * a), (-b - D**(1/2)) / (2 * a)])
-
-
 
